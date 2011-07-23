e337d3abf2c35967e4dc6ae778ae0b5ec1ab089d
danielMitD <mail@storkki.de>
2011-07-23 19:00:43 +0200

    Change comment

--- Zend/Gdata/Analytics/Extension/TableId.php
+++ Zend/Gdata/Analytics/Extension/TableId.php
@@ -10,7 +10,7 @@ require_once 'Zend/Gdata/Extension.php';
  *
  * @category   Zend
  * @package    Zend_Gdata
- * @subpackage Calendar
+ * @subpackage Analytics
  * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
